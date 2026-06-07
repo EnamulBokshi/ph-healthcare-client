7ad5b2a22d6ddfddcde2b6e2863d1344f2558569
EnamulBokshi <enamulbokshi@gmail.com>
2026-06-08 01:12:11 +0600

    feat: enhance queryRagAction for better doctor response formatting and add ingestDoctorAction

--- actions/rag.action.ts
+++ actions/rag.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { queryRagService, RagQueryPayload } from "@/services/rag.services";
+import { ingestDoctorService, queryRagService, RagQueryPayload } from "@/services/rag.services";
 
 export const queryRagAction = async (payload: RagQueryPayload) => {
   try {
@@ -23,13 +23,19 @@ export const queryRagAction = async (payload: RagQueryPayload) => {
             doctors.map((doctor: any, index: number) => {
               let text = "";
 
-              if (doctor.name) text += `Name: ${doctor.name}\n`;
+              if (doctor.name) text += `${index + 1}. ***${doctor.name}\n***`;
               if (doctor.specialty) text += `Specialty: ${doctor.specialty}\n`;
               if (doctor.reason) text += `Reason: ${doctor.reason}\n`;
 
-              return `${index + 1}. ${text}`;
+              return text;
             });
+        } else{
+          answer = "I couldn't find any doctors that match your query. Please try rephrasing your question or providing more details.";
+
         }
+      }
+      else {
+        answer = JSON.stringify(answer, null, 2);
       }
     }
     let sources = 100 - Number(response.data?.sources[0]?.similarity) * 100;
@@ -40,7 +46,48 @@ export const queryRagAction = async (payload: RagQueryPayload) => {
     };
   } catch (error) {
     console.error("Error in queryRagAction:", error);
-    throw error; // Re-throw the error to be handled by the caller
+   
+    return {
+      success: false,
+      error: "An error occurred while processing your request. Please try again later.",
+    }
   }
 };
 
+
+export const ingestDoctorAction = async () => {
+  try {
+    const response = await ingestDoctorService();
+    if (!response?.data) {
+      return {
+        success: false,
+        error: "No data found in the response",
+      };
+    }
+    return {
+      success: response.data.success,
+      message: response.data.message ?? response.message ?? "No message provided",
+      indexedCount: response.data.indexedCount,
+    };
+
+  }
+  catch (error) {
+    console.error("Error in ingestDoctorAction:", error);
+    return {
+      success:false,
+      error: "An error occurred while processing your request. Please try again later.",  
+    }
+  }
+}
+
+
+export const getUserRoleAction = async( ) => {
+  try {
+    const {getUserInfo} = await import("@/services/auth.services");
+    const userInfoResponse = await getUserInfo();
+    return userInfoResponse.role ?? null;
+  } catch (error) {
+    console.error("Error in getUserRoleAction:", error);
+    return null;
+  }
+}
